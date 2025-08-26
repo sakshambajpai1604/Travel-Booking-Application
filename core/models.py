ca79880e843eefdd848735eb9f2857ecdaef9859
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-08-26 15:12:50 +0530

    Added Booking Model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class TravelOption(models.Model):
 	TRAVEL_TYPES = [
@@ -19,17 +19,17 @@ class TravelOption(models.Model):
 		return f"{self.type} from {self.source} to {self.destination} on {self.date_time}" 
 
 class Booking(models.Model):
-	STATUS_CHOICES = [
-		('Confirmed', 'Confirmed'),
-		('Cancelled', 'Cancelled'),
-	]
-	booking_id = models.AutoField(primary_key=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	travel_option = models.ForeignKey(TravelOption, on_delete=models.CASCADE)
-	number_of_seats = models.PositiveIntegerField()
-	total_price = models.DecimalField(max_digits=10, decimal_places=2)
-	booking_date = models.DateTimeField(auto_now_add=True)
-	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Confirmed')
+    STATUS_CHOICES = [
+        ('Confirmed', 'Confirmed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    booking_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    travel_option = models.ForeignKey(TravelOption, on_delete=models.CASCADE)
+    number_of_seats = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Confirmed')
 
-	def __str__(self):
-		return f"Booking {self.booking_id} by {self.user.username}"
+    def __str__(self):
+        return f"Booking {self.booking_id} by {self.user.username}"
