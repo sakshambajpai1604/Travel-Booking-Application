2e0351256ce05f16ba34dfda4cb6788ed30d9c0a
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-08-26 15:10:03 +0530

    Added URL for travel options

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('travel-options/', views.travel_options_list, name='travel_options_list'),
 ]
