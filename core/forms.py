b189eb8b57d7a3486ed4a6238dbbc0f498a0a59b
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-08-26 15:26:17 +0530

    Created a profile update form

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class BookingForm(forms.Form):
     number_of_seats = forms.IntegerField(min_value=1, label="Number of Seats")
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
