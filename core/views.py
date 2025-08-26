bf8da41cdb130a74cb4966489a5f7e38d9f5005c
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-08-26 15:15:55 +0530

    Booking view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 from django.contrib.auth import login, authenticate, logout
@@ -8,7 +8,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django import forms
-from .models import TravelOption
+from .models import TravelOption, Booking
+from .forms import BookingForm
 
 # Profile update form
 class ProfileForm(forms.ModelForm):
@@ -59,3 +60,27 @@ def profile_view(request):
 def travel_options_list(request):
     options = TravelOption.objects.all()
     return render(request, 'core/travel_options_list.html', {'options': options})
+
+@login_required
+def book_travel(request, travel_id):
+    travel_option = get_object_or_404(TravelOption, pk=travel_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            seats = form.cleaned_data['number_of_seats']
+            if seats > travel_option.available_seats:
+                form.add_error('number_of_seats', 'Not enough seats available.')
+            else:
+                total_price = seats * travel_option.price
+                Booking.objects.create(
+                    user=request.user,
+                    travel_option=travel_option,
+                    number_of_seats=seats,
+                    total_price=total_price,
+                )
+                travel_option.available_seats -= seats
+                travel_option.save()
+                return redirect('my_bookings')
+    else:
+        form = BookingForm()
+    return render(request, 'core/book_travel.html', {'form': form, 'travel_option': travel_option})
